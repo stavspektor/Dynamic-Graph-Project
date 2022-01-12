918932dda18174fbcf40bb8d3ecc66cb9889dec9
ofer lev <oferlev5@outlook.co.il>
2022-01-12 18:15:43 +0200

    hey

--- src/DynamicGraph.java
+++ src/DynamicGraph.java
@@ -2,13 +2,13 @@ public class DynamicGraph {
     GraphNode nodeHead;
     GraphEdge edgeHead;
 
-    public DynamicGraph(){
+    public DynamicGraph() {
         DynamicGraph dynamicGraph = new DynamicGraph();
         this.nodeHead = null;
         this.edgeHead = null;
     }
 
-    public GraphNode insertNode(int nodeKey){
+    public GraphNode insertNode(int nodeKey) {
         GraphNode newNode = new GraphNode(nodeKey);
         if (this.nodeHead != null) {
             this.nodeHead.prev = newNode;
@@ -18,53 +18,50 @@ public class DynamicGraph {
         return this.nodeHead;
     }
 
-    public void deleteNode(GraphNode node){
-        if (node.inSum == 0 && node.outSum == 0){
-            if (node.next == null){ // might have mistakes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public void deleteNode(GraphNode node) {
+        if (node.inSum == 0 && node.outSum == 0) {
+            if (node.next == null) { // might have mistakes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 node.prev.next = null;
-            }
-            else if (node.prev == null){
+            } else if (node.prev == null) {
                 node.next.prev = null;
-            }
-            else {
+            } else {
                 node.prev.next = node.next;
                 node.next.prev = node.prev;
             }
         }
     }
 
-    public GraphEdge insertEdge(GraphNode from,GraphNode to){
-        GraphEdge newEdge = new GraphEdge(from,to);
+    public GraphEdge insertEdge(GraphNode from, GraphNode to) {
+        GraphEdge newEdge = new GraphEdge(from, to);
         if (this.edgeHead != null) {
             this.edgeHead.prev = newEdge;
             newEdge.next = this.edgeHead;
         }
         this.edgeHead = newEdge;
-        newEdge.to.inSum +=1;
-        newEdge.from.outSum +=1;
+        newEdge.to.inSum += 1;
+        newEdge.from.outSum += 1;
         return newEdge;
     }
 
-    public void deleteEdge(GraphEdge edge){
-        if (edge.next == null){ // might have mistakes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public void deleteEdge(GraphEdge edge) {
+        if (edge.next == null) { // might have mistakes!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             edge.prev.next = null;
-        }
-        else if (edge.prev == null){
+        } else if (edge.prev == null) {
             edge.next.prev = null;
-        }
-        else {
+        } else {
             edge.prev.next = edge.next;
             edge.next.prev = edge.prev;
         }
-        edge.to.inSum -=1;
-        edge.from.outSum -=1;
-    }
-
-    public RootedTree scc(){
-
-    }
-
-    public RootedTree bfs(GraphNode source){
-
+        edge.to.inSum -= 1;
+        edge.from.outSum -= 1;
     }
 }
+
+//    public RootedTree scc(){
+//
+//    }
+//
+//    public RootedTree bfs(GraphNode source){
+//
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static <String> void main(String[] args) {
+	DynamicGraph graph1 = new DynamicGraph();
+	graph1.insertNode(1);
+	graph1.insertNode(2);
+	GraphNode tempnode = new GraphNode(0);
+	tempnode = graph1.nodeHead;
+	while (tempnode.next!=null) {
+		System.out.println(graph1.nodeHead.key);
+		System.out.println("--->");
+		tempnode = tempnode.next;
+	}
     }
 }
