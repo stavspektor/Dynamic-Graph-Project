09a4d5a603138daa8f899261c2a2a213eca630ce
stavspektor <stavspektor@gmail.com>
2022-01-12 18:23:30 +0200

    temporary

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
 
-    public static <String> void main(String[] args) {
+    public static void main(String[] args) {
 	DynamicGraph graph1 = new DynamicGraph();
 	graph1.insertNode(1);
 	graph1.insertNode(2);
 	GraphNode tempnode = new GraphNode(0);
 	tempnode = graph1.nodeHead;
 	while (tempnode.next!=null) {
-		System.out.println(graph1.nodeHead.key);
+		System.out.println(tempnode.key);
 		System.out.println("--->");
 		tempnode = tempnode.next;
 	}
+	System.out.println(tempnode.key);
     }
 }
